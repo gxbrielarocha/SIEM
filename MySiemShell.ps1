827d8813e4214f3bfc2ef4ef9403c9023577a686
Gabriel Arocha <143841331+gxbrielarocha@users.noreply.github.com>
2023-09-19 23:04:05 -0400

    Add files via upload
    This is the code I am using for my Automatic error detection for my PC.

---
+++ MySiemShell.ps1
@@ -0,0 +1,19 @@
+﻿#List of servers to get events from
+$servers = "GabesPC"
+
+#Search Parameters
+$searchFields = @{
+    "LogName" = "System"
+    "ID" = 12
+}
+
+#Create empty object to store list of events
+$allEvents = @()
+
+#Loop through the server list
+foreach($xyz in $servers) {
+    $allEvents += Get-WinEvent -FilterHashtable $searchFields -ComputerName $xyz
+}
+#Export allEvents into a CSV excell file
+$allEvents | Export-CSV -Path "C:\Users\gabri\OneDrive\Desktop\mysiem.csv"
+
